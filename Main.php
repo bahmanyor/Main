2ce39fbbb69ea1759651b5f2ec73937f39fc07dd
bahmanyor <b.soliev@team.alif.tj>
2021-01-23 09:51:12 +0500

    first commit

---
+++ Main.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace APP;
+
+
+
+class Main {
+
+     public function __construct() {
+
+     }
+
+     public function main(){
+          echo "Hello world";
+     }
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+use APP\Main;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+(new Main())->main();
